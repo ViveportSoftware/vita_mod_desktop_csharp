b2d99833c7b08cd4840aa550875b7b75564d0245
Kene Lin <kenelin@gmail.com>
2018-03-26 23:19:18 +0800

    Minor update

--- source/Htc.Vita.Mod.Desktop/BouncyCastle/Md5Impl.cs
+++ source/Htc.Vita.Mod.Desktop/BouncyCastle/Md5Impl.cs
@@ -18,6 +18,53 @@ namespace Htc.Vita.Mod.Desktop.BouncyCastle
             _logger = Logger.GetInstance();
         }
 
+        private static string DoGenerateInBase64(FileInfo file)
+        {
+            return Core.Util.Convert.ToBase64String(GetDigestInByteArray(file));
+        }
+
+        private static string DoGenerateInBase64(string content)
+        {
+            return Core.Util.Convert.ToBase64String(GetDigestInByteArray(content));
+        }
+
+        private static string DoGenerateInHex(FileInfo file)
+        {
+            return Core.Util.Convert.ToHexString(GetDigestInByteArray(file));
+        }
+
+        private static string DoGenerateInHex(string content)
+        {
+            return Core.Util.Convert.ToHexString(GetDigestInByteArray(content));
+        }
+
+        private static byte[] GetDigestInByteArray(FileInfo file)
+        {
+            using (var readStream = file.OpenRead())
+            {
+                var digest = new MD5Digest();
+                var output = new byte[digest.GetDigestSize()];
+                var buffer = new byte[131072]; // 128K
+                int read;
+                while ((read = readStream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    digest.BlockUpdate(buffer, 0, read);
+                }
+                digest.DoFinal(output, 0);
+                return output;
+            }
+        }
+
+        private static byte[] GetDigestInByteArray(string content)
+        {
+            var digest = new MD5Digest();
+            var input = Encoding.UTF8.GetBytes(content);
+            var output = new byte[digest.GetDigestSize()];
+            digest.BlockUpdate(input, 0, input.Length);
+            digest.DoFinal(output, 0);
+            return output;
+        }
+
         protected override string OnGenerateInBase64(FileInfo file)
         {
             if (UsingBouncyCastleFirst)
@@ -92,53 +139,6 @@ namespace Htc.Vita.Mod.Desktop.BouncyCastle
                 UsingBouncyCastleFirst = true;
             }
             return DoGenerateInHex(content);
-        }
-
-        private static string DoGenerateInBase64(FileInfo file)
-        {
-            return Core.Util.Convert.ToBase64String(GetDigestInByteArray(file));
-        }
-
-        private static string DoGenerateInBase64(string content)
-        {
-            return Core.Util.Convert.ToBase64String(GetDigestInByteArray(content));
-        }
-
-        private static string DoGenerateInHex(FileInfo file)
-        {
-            return Core.Util.Convert.ToHexString(GetDigestInByteArray(file));
-        }
-
-        private static string DoGenerateInHex(string content)
-        {
-            return Core.Util.Convert.ToHexString(GetDigestInByteArray(content));
-        }
-
-        private static byte[] GetDigestInByteArray(FileInfo file)
-        {
-            using (var readStream = file.OpenRead())
-            {
-                var digest = new MD5Digest();
-                var output = new byte[digest.GetDigestSize()];
-                var buffer = new byte[131072]; // 128K
-                int read;
-                while ((read = readStream.Read(buffer, 0, buffer.Length)) > 0)
-                {
-                    digest.BlockUpdate(buffer, 0, read);
-                }
-                digest.DoFinal(output, 0);
-                return output;
-            }
-        }
-
-        private static byte[] GetDigestInByteArray(string content)
-        {
-            var digest = new MD5Digest();
-            var input = Encoding.UTF8.GetBytes(content);
-            var output = new byte[digest.GetDigestSize()];
-            digest.BlockUpdate(input, 0, input.Length);
-            digest.DoFinal(output, 0);
-            return output;
         }
     }
 }
