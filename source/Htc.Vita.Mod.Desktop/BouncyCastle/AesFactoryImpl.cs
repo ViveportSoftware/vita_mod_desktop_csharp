e30c5b6f8e15e1eb102a6a8546c41765d925f848
Kene Lin <kenelin@gmail.com>
2018-08-27 16:10:10 +0800

    Add BouncyCastle implementation for AES

---
+++ source/Htc.Vita.Mod.Desktop/BouncyCastle/AesFactoryImpl.cs
@@ -0,0 +1,16 @@
+﻿using Htc.Vita.Core.Crypto;
+
+namespace Htc.Vita.Mod.Desktop.BouncyCastle
+{
+    public class AesFactoryImpl : AesFactory
+    {
+        protected override Aes OnGet(Aes.CipherMode cipherMode, Aes.PaddingMode paddingMode)
+        {
+            return new AesImpl
+            {
+                    Cipher = cipherMode,
+                    Padding = paddingMode
+            };
+        }
+    }
+}
