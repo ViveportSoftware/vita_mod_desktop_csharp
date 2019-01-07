a874c636481adecc4299deeb58fd6ef627a961ad
Kene Lin <kenelin@gmail.com>
2019-01-07 22:22:57 +0800

    Add TraceListenerImpl to support system trace

---
+++ source/Htc.Vita.Mod.Desktop/Log4Net/TraceListenerImpl.cs
@@ -0,0 +1,24 @@
+﻿using System.Diagnostics;
+using Htc.Vita.Core.Log;
+using log4net.Util;
+
+namespace Htc.Vita.Mod.Desktop.Log4Net
+{
+    public class TraceListenerImpl : TraceListener
+    {
+        public TraceListenerImpl()
+        {
+            LogLog.EmitInternalMessages = false;
+        }
+
+        public override void Write(string message)
+        {
+            Logger.GetInstance(typeof(TraceListenerImpl)).Info(message);
+        }
+
+        public override void WriteLine(string message)
+        {
+            Logger.GetInstance(typeof(TraceListenerImpl)).Info(message);
+        }
+    }
+}
